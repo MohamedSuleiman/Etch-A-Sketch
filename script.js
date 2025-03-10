38cf4cc254142572b1f89ea11c6624ee489bbb6f
MohamedSuleiman <600801@stud.hvl.no>
2025-03-10 17:34:27 +0100

    Add button for changing number of grids

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const element = document.getElementById("container");
-function init() {
-    for (let i = 0; i < 16; i++) {
+let startingInt = 16;
+height = element.clientHeight / startingInt;
+
+function init(num, height) {
+    for (let i = 0; i < num; i++) {
         const el = document.createElement("div");
         el.style.display = "flex";
-        for (let j = 0; j < 16; j++) {
+        for (let j = 0; j < num; j++) {
             const div = document.createElement("div");
-            div.style.width = "50px";
-            div.style.height = "50px";
+            div.style.height = height + "px";
             div.style.border = "2px solid black";
+            div.style.flexGrow = "1";
             div.classList.add("grid");
             el.appendChild(div);
         }
@@ -18,7 +21,27 @@ function init() {
 
 element.addEventListener("mouseover",(e) => {
     const element = e.target;
-    element.style.background = "blue";
+    if (element.classList.contains("grid")) {
+        element.style.background = "blue";
+    }
 });
 
-init();
+init(startingInt, height);
+
+const setGridSize = document.getElementById("gridSize");
+
+setGridSize.addEventListener('click', gridSize);
+
+
+function gridSize() {
+    let userInput = prompt("Enter grid size up to 100 tiles: ");
+    
+    if (userInput > 100) {
+        alert("Grid size cant be bigger than 100!!")
+    } else {
+        startingInt = userInput;
+        element.replaceChildren();
+        height = element.clientHeight / startingInt;
+        init(startingInt, height);
+    }
+}
